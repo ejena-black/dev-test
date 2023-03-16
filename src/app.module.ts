ea48ce9d90be7a946f12ac6c09ee882464618d8b
Nathan Onofeghara <nathanonofeghara@gmail.com>
2023-03-16 16:06:17 +0100

    update to packages and app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './modules/auth/user.module';
+import { typeOrmConfigAsync } from './config/typeorm.config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'dev-test',
-      entities: [__dirname + '/../**/*.entity.ts{.ts,.js}'],
-      synchronize: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
